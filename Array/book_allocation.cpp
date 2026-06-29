4c65942f8fce2e93f994fb3d1f5726fe4e17288e
Rushi <rushisurose83@gmail.com>
2026-06-29 20:44:00 +0530

    Array\book_allocation.cpp

--- Array/book_allocation.cpp
+++ Array/book_allocation.cpp
@@ -52,57 +52,57 @@
 //     return 0;
 // }
 
-#include<iostream>
-#include<vector>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
 
-bool isValid(vector<int> arr , int n , int m,int maxAllowedBooks){
-    int student = 1 , book = 0 ;
-    for (int i = 0; i < n; i++)
-    {
-        if (book+arr[i]<=maxAllowedBooks)
-        {
-            book += arr[i];
-        }
-        else {
-            book = arr[i];
-            student++ ;
-        }
+// bool isValid(vector<int> arr , int n , int m,int maxAllowedBooks){
+//     int student = 1 , book = 0 ;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (book+arr[i]<=maxAllowedBooks)
+//         {
+//             book += arr[i];
+//         }
+//         else {
+//             book = arr[i];
+//             student++ ;
+//         }
 
-    }
-    return student <= m ;
+//     }
+//     return student <= m ;
 
-}
+// }
 
-int allocation(vector<int> arr , int n , int m){
-    int strt = 0 , sum = 0 , ans = -1 ;
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    int end = sum;
-    while (strt <= end)
-    {
-        int mid = strt + (end-strt)/2;
-        if(isValid(arr,n,m,mid)){
-            end = mid - 1 ;
-            ans = mid ;
-        }
-        else {
-            strt = mid + 1 ;
-        }
-    }
-    return ans ;
+// int allocation(vector<int> arr , int n , int m){
+//     int strt = 0 , sum = 0 , ans = -1 ;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += arr[i];
+//     }
+//     int end = sum;
+//     while (strt <= end)
+//     {
+//         int mid = strt + (end-strt)/2;
+//         if(isValid(arr,n,m,mid)){
+//             end = mid - 1 ;
+//             ans = mid ;
+//         }
+//         else {
+//             strt = mid + 1 ;
+//         }
+//     }
+//     return ans ;
 
-}
+// }
                  
-int main()
-{
-    vector<int> arr = {25, 46, 28, 49, 24};
-    int m = 4;
-    cout << findPages(arr, m);
-    return 0;
-}
+// int main()
+// {
+//     vector<int> arr = {25, 46, 28, 49, 24};
+//     int m = 4;
+//     cout << findPages(arr, m);
+//     return 0;
+// }
 
 #include <iostream>
 #include <vector>
